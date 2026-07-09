0065147c0d09c3e7a66110deda80bc9bf64c26b4
Yousuf7900 <yousufislam7900@gmail.com>
2026-07-09 22:11:15 +0600

    Error code problem solved

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,7 +1,7 @@
 import type { ErrorRequestHandler } from "express";
 import httpStatus from "http-status";
 import { ZodError } from "zod";
-import AppError from "../error/globalError";
+import GlobalError from "../error/globalError";
 
 export const globalErrorHandler: ErrorRequestHandler = (
     err,
@@ -10,13 +10,13 @@ export const globalErrorHandler: ErrorRequestHandler = (
     next
 ) => {
 
-    let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+    let statusCode: number = httpStatus.INTERNAL_SERVER_ERROR;
     let message = "Something went wrong";
     let errorDetails: unknown = null;
 
     if (err instanceof ZodError) {
 
-        let statusCode: number = httpStatus.BAD_REQUEST;
+        statusCode = httpStatus.BAD_REQUEST;
         message = "Validation Error";
 
         errorDetails = err.issues.map(issue => ({
@@ -24,22 +24,20 @@ export const globalErrorHandler: ErrorRequestHandler = (
             message: issue.message
         }));
 
-    }
+    } else if (err instanceof GlobalError) {
 
-    else if (err instanceof AppError) {
-
-        let statusCode: number = err.statusCode;
+        statusCode = err.statusCode;
         message = err.message;
 
-    }
-
-    else if (err instanceof Error) {
+    } else if (err instanceof Error) {
 
         message = err.message;
+
     }
 
     res.status(statusCode).json({
         success: false,
+        statusCode,
         message,
         errorDetails
     });
